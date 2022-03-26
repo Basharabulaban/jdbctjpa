1019cb4c3c4d735695710a78178326bfd0d857e8
Basharabulaban <ba.ahmad@gmail.com>
2022-03-26 17:04:25 +0300

    and insert , update
    Alos implment rowmapper

--- jdbc-to-jpa/src/main/java/com/densin/rws/jdbctojpa/JdbcToJpaApplication.java
+++ jdbc-to-jpa/src/main/java/com/densin/rws/jdbctojpa/JdbcToJpaApplication.java
@@ -2,12 +2,17 @@ package com.densin.rws.jdbctojpa;
 
 
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.densin.rws.jdbctojpa.entity.Person;
+
 import net.bytebuddy.asm.Advice.This;
 
 @SpringBootApplication
@@ -29,7 +34,11 @@ public class JdbcToJpaApplication implements CommandLineRunner {
 		logger.info("users by name -> {} ",	dao.findbyname("bashar"));
 		logger.info("number of ROWS DELETED -> - {} ",		dao.deletebyname("bashar"));
 		logger.info("users by name -> {} ",	dao.findbyname("bashar"));
-		logger.info("delete name based on id and name -> - ",	dao.deleteby_ID_name(2,"Ahmad"));
+		logger.info("uinsert by Person  -> {} ",	dao.insert_by_object(new Person(5,"Abood","Bagdad",new Date())));
+//		logger.info("delete name based on id and name -> - ",	dao.deleteby_ID_name(2,"Ahmad"));
+		logger.info("All Users -> {}",	dao.findAll());
+		logger.info("update by name -> {} ",	dao.update_by_id(new Person(5,"Sami","Bagdad",new Date())));
+//		logger.info("insert name based on id and name -> - ",	dao.insertrecord(6,"Ahmad","Riyadh",new Date()));
 		logger.info("All Users -> {}",	dao.findAll());
 	}
 
--- jdbc-to-jpa/src/main/java/com/densin/rws/jdbctojpa/PersonJdbcDao.java
+++ jdbc-to-jpa/src/main/java/com/densin/rws/jdbctojpa/PersonJdbcDao.java
@@ -1,12 +1,18 @@
 package com.densin.rws.jdbctojpa;
 
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
 
 import com.densin.rws.jdbctojpa.entity.Person;
 
@@ -15,9 +21,25 @@ public class PersonJdbcDao{
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 	
+	
+	class PersonRowMapper implements RowMapper<Person>{
+
+		@Override
+		public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Person person= new Person();
+			
+			person.setId(rs.getInt("id"));	
+			person.setName(rs.getString("name"));	
+			person.setLocation(rs.getString("location"));	
+			person.setBirthDate(rs.getTimestamp("birth_date"));	
+			return person;
+		}
+		
+	}
+	// you can chnage new BeanPropertyRowMapper<Person>(Person.class)        with       new PersonRowMapper()
 	//select * from person
 	public List<Person> findAll(){
-	return	jdbcTemplate.query("select * from person",new BeanPropertyRowMapper(Person.class));
+	return	jdbcTemplate.query("select * from person",new PersonRowMapper());
 		
 	}
 	
@@ -40,8 +62,6 @@ public class PersonJdbcDao{
 		return	jdbcTemplate.query
 				("select * from person where name=?", new Object[] {name},
 						new BeanPropertyRowMapper<Person>(Person.class) ) ;
-				
-			
 		}
 
 	public int deletebyname(String name) {
@@ -55,6 +75,25 @@ public class PersonJdbcDao{
 				("delete from person where id =? and name=?", new Object[] {id,name} ) ;
 	}
 
+	public Object insertrecord(int id, String name, String location, Date birth_date) {
+		return	jdbcTemplate.update
+		("insert into person  (id,name,location,birth_date) values(?,?,?,?)" , new Object[] {id,name,location,birth_date} ) ;
+
+	
+	}
+	
+	public Object insert_by_object(Person person) {
+		return	jdbcTemplate.update
+		("insert into person  (id,name,location,birth_date) values(?,?,?,?)" , new Object[] {person.getId(),person.getName(),person.getLocation(),new Timestamp(person.getBirthDate().getTime()) } ) ;
+
+	
+	}
+
+	public Object update_by_id(Person person) {
+		return	jdbcTemplate.update
+		("update person  set   name= ?,location= ?,birth_date= ?   where id= ?" , new Object[] {person.getName(),person.getLocation(),new Timestamp(person.getBirthDate().getTime()) ,person.getId() } ) ;
+	}
+	
 
 	
 	
